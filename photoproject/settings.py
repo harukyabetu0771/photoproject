70d71cf5041c351b422281392fb4e6048276df3f
harukyabetu0771 <mit2571502@stu.o-hara.ac.jp>
2025-10-28 12:39:28 +0900

    Update settings.py

--- photoproject/settings.py
+++ photoproject/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gol(%4%t#_jj3kfpdb4hgc(r637=ri53@5szmn%q2gtk%m$sa4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['photosystem.pythonanywhere.com']
 
 
 # Application definition
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=(os.path.join(BASE_DIR,'static'),)
 
 # Default primary key field type
